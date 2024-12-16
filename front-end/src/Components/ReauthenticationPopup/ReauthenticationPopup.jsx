3b88b3336d6ac5d5b793b55dc4357e8def273ecd
Stephen0wen <stevejowen@gmail.com>
2024-12-16 22:39:28 +0000

    Allows deleting an account when signed in with email and password

--- front-end/src/Components/ReauthenticationPopup/ReauthenticationPopup.jsx
+++ front-end/src/Components/ReauthenticationPopup/ReauthenticationPopup.jsx
@@ -3,7 +3,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { deleteUser } from "../../apiRequests";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ErrorContext } from "../../Contexts/ErrorContext";
 import { UserContext } from "../../Contexts/UserContext";
 
@@ -13,7 +13,8 @@ export default function ReauthenticationPopup({
     provider,
 }) {
     const { setError } = useContext(ErrorContext);
-    const { token } = useContext(UserContext);
+    const { token, user } = useContext(UserContext);
+    const [password, setPassword] = useState("");
     const nagivate = useNavigate();
 
     const googleReauth = () => {
@@ -22,6 +23,17 @@ export default function ReauthenticationPopup({
             .currentUser.reauthenticateWithPopup(
                 new firebase.auth.GoogleAuthProvider()
             );
+    };
+
+    const passwordReauth = () => {
+        const { email } = firebase.auth().currentUser._delegate;
+        const credential = firebase.auth.EmailAuthProvider.credential(
+            email,
+            password
+        );
+        return firebase
+            .auth()
+            .currentUser.reauthenticateWithCredential(credential);
     };
 
     const googleDelete = () => {
@@ -38,6 +50,26 @@ export default function ReauthenticationPopup({
             .catch((error) => {
                 setError(error);
             });
+    };
+
+    const passwordDelete = () => {
+        passwordReauth()
+            .then(() => {
+                return deleteUser(token);
+            })
+            .then(() => {
+                return firebase.auth().currentUser.delete();
+            })
+            .then(() => {
+                nagivate("/");
+            })
+            .catch((error) => {
+                setError(error);
+            });
+    };
+
+    const updatePassword = ({ target: { value } }) => {
+        setPassword(value);
     };
 
     if (provider === "google.com") {
@@ -57,6 +89,44 @@ export default function ReauthenticationPopup({
                             Cancel
                         </button>
                         <button className="danger" onClick={googleDelete}>
+                            Delete Account
+                        </button>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+
+    if (provider === "password") {
+        return (
+            <div id="grey-zone" className={isHidden ? "hidden" : ""}>
+                <section id="reauth-popup">
+                    <p id="reauth-message">
+                        Are you sure you want to delete your account and all
+                        associated data? You must enter your password to
+                        confirm...
+                    </p>
+                    <form>
+                        <p>{user.password}</p>
+                        <label>
+                            <p>Password</p>
+                            <input
+                                id="password-input"
+                                type="password"
+                                onChange={updatePassword}
+                                value={password}
+                            />
+                        </label>
+                    </form>
+                    <div id="reauth-button-container">
+                        <button
+                            onClick={() => {
+                                setIsHidden(true);
+                            }}
+                        >
+                            Cancel
+                        </button>
+                        <button className="danger" onClick={passwordDelete}>
                             Delete Account
                         </button>
                     </div>
